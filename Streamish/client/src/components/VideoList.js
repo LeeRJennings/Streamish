6df7e2e2b8b4183d3ddae8fdc1b443e8f0c0fce0
LeeRJennings <leerjennings@outlook.com>
2022-07-07 14:33:47 -0500

    react hooked up

---
+++ Streamish/client/src/components/VideoList.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import { getAllVideos } from "../modules/videoManager";
+
+const VideoList = () => {
+  const [videos, setVideos] = useState([]);
+
+  const getVideos = () => {
+    getAllVideos()
+    .then(videos => setVideos(videos));
+  };
+
+  useEffect(() => {
+    getVideos();
+  }, []);
+
+  return (
+    <div>
+      {videos.map(v => 
+        <div>{v.title}</div>
+      )}
+    </div>
+  );
+}
+
+export default VideoList;
