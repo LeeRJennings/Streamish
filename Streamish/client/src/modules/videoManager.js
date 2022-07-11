a5a3cd86a27d7452a19fd34b59032fdd67d270f0
LeeRJennings <leerjennings@outlook.com>
2022-07-11 15:23:23 -0500

    routing chapter complete

--- Streamish/client/src/modules/videoManager.js
+++ Streamish/client/src/modules/videoManager.js
@@ -23,4 +23,9 @@ export const addVideo = (video) => {
     },
     body: JSON.stringify(video),
   });
+
+};
+
+export const getVideo = (id) => {
+  return fetch(`${baseUrl}/GetWithComments/${id}`).then((res) => res.json());
 };
